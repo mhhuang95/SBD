021ac88157df99674498576c79390f23237f3f9d
mhhuang95 <mhhuang@ucdavis.edu>
2019-02-26 10:44:08 -0800

    compare annealing and manpg '

--- Compare_man_ane.py
+++ Compare_man_ane.py
@@ -5,13 +5,16 @@ import time
 
 def maxdoshift(a0, a):
     return np.max(np.abs(np.correlate(a0, a, "full")))
+
+
+
 class ManPG(object):
     def __init__(self, m, k,x0,a0,x,a):
         self.m = m
         self.k = k
         self.theta = 1 / k
-        self.max_iter = 1000
-        self.epsilon = 1e-7
+
+        self.epsilon = 1e-5
 
         self.a0 = a0
         self.x0 = x0
@@ -20,8 +23,8 @@ class ManPG(object):
         self.a = a
         self.x = x
 
-        self.lams = [0.1]
-        self.lam = 0.1
+        #self.lams = [0.5]
+        self.lam = 0.5
 
     def init_a(self):
         start = np.random.randint(0, self.m)
@@ -97,15 +100,15 @@ class ManPG(object):
 
         self.costs = [obj]
 
-        for lam in self.lams:
-            self.lam = lam
-            i = 0
-            g_a = np.ones(self.a.shape[0])
-            t = 0.1
-            while i < self.max_iter and np.linalg.norm(g_a)**2 * t > self.epsilon:
-                g_a, t,obj = self.step()
-                i+=1
-                self.costs.append(obj)
+        #for lam in self.lams:
+        #   self.lam = lam
+        i = 0
+        g_a = np.ones(self.a.shape[0])
+        t = 0.1
+        while np.linalg.norm(g_a)**2 * t > self.epsilon:
+            g_a, t,obj = self.step()
+            i+=1
+            self.costs.append(obj)
         #print("--- %s seconds ---" % (time.time() - start))
         '''
         while 1 - maxdoshift(self.a0, self.a) > 0.0001:
@@ -118,14 +121,13 @@ class anealing(object):
         self.m = x0.shape[0]
         self.k = a0.shape[0]
         self.theta = 1 / self.k
-        self.max_iter = 1000
-        self.epsilon = 1e-3
+        self.epsilon = 1e-5
         self.x0 = x0
         self.a0 = a0
         self.y =  np.real(np.fft.ifft(np.fft.fft(x0)*np.fft.fft(a0,m)))
         self.yhat = np.fft.fft(self.y)
-        self.lams = [0.1]
-        self.lam = 0.1
+        #self.lams = [0.1]
+        self.lam = 0.5
         self.x = x
         self.a = a
 
@@ -202,21 +204,22 @@ class anealing(object):
         obj = 0.5 * np.linalg.norm(np.real(np.fft.ifft(ahat * xhat)) - self.y) ** 2 + self.lam * np.sum(np.abs(self.x))
         self.costs = [obj]
 
-        self.a = self.calc_grad()[0]
+        #self.a = self.calc_grad()[0]
         #print(self.a)
-        self.a = -1*self.a
-        self.a /= np.linalg.norm(self.a)
+        #self.a = -1*self.a
+        #self.a /= np.linalg.norm(self.a)
 
-        for lam in self.lams:
-            self.lam = lam
-            i = 0
-            g_a = np.ones(self.a.shape[0])
-            t = 0.1
-            while i < self.max_iter and np.linalg.norm(g_a)**2 * t > self.epsilon:
+        #for lam in self.lams:
+        #    self.lam = lam
+        i = 0
+        g_a = np.ones(self.a.shape[0])
+        t = 0.1
+        #while i < self.max_iter and np.linalg.norm(g_a)**2 * t > self.epsilon:
+        while np.linalg.norm(g_a) ** 2 * t > self.epsilon:
             #while i < self.max_iter:
-                g_a, cost,t,obj = self.step()
-                i+=1
-                #self.costs.append(obj)
+            g_a, cost,t,obj = self.step()
+            i+=1
+            #self.costs.append(obj)
         #print("--- %s seconds ---" % (time.time() - start))
         '''
         while 1 - maxdoshift(self.a0, self.a) > 0.0001:
@@ -233,11 +236,16 @@ def init_a(m,k,y):
 
 if __name__ == "__main__":
 
-    '''
-    k=200
-    m=100000
+
+    k=2000
+    m=1000000
+
+    obj_ann = []
+    obj_man = []
     accu_ane = []
     accu_man = []
+    xerr_ann = []
+    xerr_man = []
     time_ane = []
     time_man = []
     iter_ane = []
@@ -256,27 +264,32 @@ if __name__ == "__main__":
         start1 = time.time()
         s1.solve()
         t_anealing = time.time() - start1
+        obj_ann.append(s1.costs[-1])
         time_ane.append(t_anealing)
         accu_ane.append(1-maxdoshift(s1.a0, s1.a))
+        xerr_ann.append(np.linalg.norm(np.abs(x0) - np.abs(s1.x))/np.linalg.norm(x0))
 
         s2 = ManPG(m,k,x0,a0,x,a)
         start2 = time.time()
         s2.solve()
         t_manpg = time.time() - start2
+        obj_man.append(s2.costs[-1])
         time_man.append(t_manpg)
         accu_man.append(1-maxdoshift(s2.a0, s2.a))
-        print(i + 1, '& %.6f'%t_manpg,'& %.6f'%t_anealing, '& %f'%(1- maxdoshift(s2.a0, s2.a)),'& %f'%(1-maxdoshift(s1.a0, s1.a)),'&',len(s2.costs),'&',len(s1.costs))
+        xerr_man.append(np.linalg.norm(np.abs(x0) - np.abs(s2.x)) / np.linalg.norm(x0))
+
+        print(i + 1,'&%.4e'%s1.costs[-1], '&%.4e'%s2.costs[-1],'&%.4f'% t_manpg,'&%.4f'% t_anealing, '&%.4e'%(1- maxdoshift(s2.a0, s2.a)),'&%.4e'%(1-maxdoshift(s1.a0, s1.a)),'&%.2f'%xerr_man[-1],'&%.2f'%xerr_ann[-1],'&', len(s2.costs),'&',len(s1.costs))
         #print(i+1,'&',len(s2.costs),'&',len(s1.costs))
         iter_ane.append(len(s1.costs))
         iter_man.append(len(s2.costs))
 
 
-    print("Average",'& %.6f'%(sum(time_man) / len(time_man)),'& %.6f'%(sum(time_ane) / len(time_ane)),'& %f'%(sum(accu_man) / len(accu_man)),'& %f'%(sum(accu_ane) / len(accu_ane)),'&',sum(iter_man) / len(iter_man),'&', sum(iter_ane) / len(iter_ane) )
+    print("Ave",'&%.4e'% (sum(obj_man) / len(obj_man)), '&%.4e'%(sum(obj_ann) / len(obj_ann)), '& %.4f'%(sum(time_man) / len(time_man)),'&%.4f'%(sum(time_ane) / len(time_ane)),'&%.4e'%(sum(accu_man) / len(accu_man)),'&%.4e'% (sum(accu_ane) / len(accu_ane)),'&%.2f'%(sum(xerr_man)/len(xerr_man)),'&%.2f'%(sum(xerr_ann)/len(xerr_ann)), '&',sum(iter_man) / len(iter_man),'&', sum(iter_ane) / len(iter_ane) )
     #print("Average",'&',sum(iter_man) / len(iter_man),'&', sum(iter_ane) / len(iter_ane))
 
     '''
-    m = 10000
-    k = 20
+    m = 100000
+    k = 200
     a0 = np.random.randn(k)
     a0 /= np.linalg.norm(a0)
     theta = 1 / k
@@ -287,7 +300,7 @@ if __name__ == "__main__":
     s1 = anealing(x0, a0, x, a)
     s1.solve()
     print("ane Kernel a: max_i|<s_i[a_0],a>| = ", maxdoshift(s1.a0, s1.a))
-    print(np.linalg.norm(s1.x0 - s1.x)/np.linalg.norm(s1.x0))
+    print(np.linalg.norm(np.abs(s1.x0) - np.abs(s1.x))/np.linalg.norm(s1.x0))
     per = 0
     for l in s1.x:
         if abs(l) < 1e-3:
@@ -298,22 +311,22 @@ if __name__ == "__main__":
 
     s2.solve()
     print("man Kernel a: max_i|<s_i[a_0],a>| = ", maxdoshift(s2.a0, s2.a))
-    print(np.linalg.norm(s2.x0 - s2.x)/np.linalg.norm(s2.x0))
+    print(np.linalg.norm(np.abs(s2.x0) - np.abs(s2.x))/np.linalg.norm(s2.x0))
     per = 0
     for l in s2.x:
         if abs(l) < 1e-3:
             per += 1
     print(per/m)
-    print(s2.costs[-1])
+    #print(s2.costs[-1])
 
     print(s1.costs[len(s2.costs)-1])
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.semilogy(s1.costs, 'b-', label='Anealing')
     ax.semilogy(s2.costs, 'r-', label='ManPG')
-    plt.title("Objective function for ManPG and Anealing, m=1000000,k=2000")
+    plt.title("Obj for ManPG and Annealing, m=100000,k=200, lambda = 0.05")
     plt.xlabel("Iteration")
     plt.ylabel("Objective function")
     plt.legend()
     plt.show()
-
+    '''
